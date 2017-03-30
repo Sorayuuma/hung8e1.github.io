2363a7dd5688be9714cd996adfaea2cd735a94fe
Hung Nguyen <hunghill0502@gmail.com>
2017-03-30 14:31:56 +0000

    Changing the format of geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,4 +1,4 @@
-var NUM_CIRCLES = 12;
+var NUM_CIRCLES = 14;
 var cicleDiameter;
 var circleRadius;
 var rVal;
@@ -8,10 +8,10 @@ var bVal;
 function setup() {
     createCanvas(480, 600);
     
-    frameRate(5);
+    frameRate(9);
     
     circleDiameter = width/NUM_CIRCLES;
-    circleRadius = circleDiameter/2;
+    circleRadius = circleDiameter/6;
     
     rVal = 255;
     gVal = 0;
@@ -30,7 +30,7 @@ function draw() {
     if (isShifted) {
       x = circleRadius;
     } else {
-      x = 0;
+      x = 45;
     }
 
     while (x <= width) {
