a68412bf13e185004eb3a6cac889964fd753acfc
Hung Nguyen <hunghill0502@gmail.com>
2017-03-30 13:41:28 +0000

    Add save keybinding to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -51,3 +51,10 @@ function draw() {
     bVal = bVal + 3;
   }
 }
+
+function keyPressed() {
+  if (keyCode === 115 || keyCode === 83) {
+    saveCanvas('geometricPattern', 'png');
+  }
+  return false;
+}
